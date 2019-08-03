0e9abe9d80544321cdc7c2874b4201ba4761a13d
kamils224 <kamil.sagalara@student.put.poznan.pl>
2019-08-03 21:53:32 +0200

    document details

--- IeasteJson2Svg/Models/SvgDocument.cs
+++ IeasteJson2Svg/Models/SvgDocument.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,12 @@ namespace IeasteJson2Svg.Models
     public class SvgDocument
     {
         public int ID { get; set; }
+        [Display(Name = "Name")]
         public string DocumentName { get; set; }
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+        [Display(Name = "Path")]
         public string DocumentPath { get; set; }
+        
     }
 }
